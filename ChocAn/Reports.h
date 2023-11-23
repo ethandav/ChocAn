62054586028cdbad8a66bbec4b366095c57447b5
Aaron <aaronmai4@gmail.com>
2023-11-22 23:43:44 -0800

    Nothing set in stone, starter stubs

--- ChocAn/Reports.h
+++ ChocAn/Reports.h
@@ -1,9 +1,24 @@
 #pragma once
+#include <string>
+#include <list>
 
 class Reports
 {
 public:
+
 	Reports() {};
 	~Reports() {};
+	// weekly reports
+	void generateMemberReports(const std::list<Member>& members);	
+	void generateProviderReports(const std::list<Provider>& providers);	
+	// manual reports
+	void generateMemberReportById(const std::string & memberId);
+	void generateProviderReportById(const std::string& providerId);
+
+
 private:
+	// helper functions for generateReportById
+	Member * findMember(const std::string& id);
+	Provider * findProvider(const std::string& id);
+
 };
